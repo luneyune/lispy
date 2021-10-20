91453039e740342319523d0a1d12509bc85c596e
Lune Yune <klass3389@gmail.com>
2021-10-20 21:52:19 +0700

    Added usage of editline library

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+/* Declare a buffer for user input of size 2048 */
+static char input[2048];
 
 int main(int argc, char** argv)
+
 {
-    printf("pass\n");
-    return 0;
+
+    /* Print Version and Exit Information */
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    /* In a never ending loop */
+    while (1) {
+
+    /* Output our prompt */
+    char* input = readline("lispy> ");
+
+    /* Read a line of user input of maximum size 2048 */
+    add_history(input);
+
+    /* Echo input back to user */
+    printf("No you're a %s\n", input);
+
+    free(input);
+  }
+
+  return 0;
 }
